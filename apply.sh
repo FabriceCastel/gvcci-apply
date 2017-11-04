4adc79ae3a66b3a473783742f625d899e8962953
Fabrice Castel <fabrice.castel@hotmail.com>
2017-11-04 16:41:41 -0400

    make sure this script works no matter where it's being invoked

--- apply.sh
+++ apply.sh
@@ -5,13 +5,15 @@ theme_name=$1
 terminal_name=$2
 img_path=$(cd ~/.gvcci/themes/$theme_name; pwd)/$(ls ~/.gvcci/themes/"$theme_name"/ | grep wallpaper)
 
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 case $platform in
     darwin*)
-        ./wallpaper/macOS.sh $img_path
+        $current_dir/wallpaper/macOS.sh $img_path
         ;;
     *)
         echo "Could not find script to apply wallpaper to platform $platform"
         ;;
 esac
 
-./terminal/"$terminal_name".sh $theme_name
+$current_dir/terminal/"$terminal_name".sh $theme_name
