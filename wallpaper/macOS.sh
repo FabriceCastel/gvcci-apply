b91d44aeaa46b89692b49fc3d18bea7c1d55fd3f
Fabrice Castel <fabrice.castel@hotmail.com>
2017-11-10 13:54:15 -0500

    the macOS wallpaper apply script should work with multiple desktops as much as possible. this should make all currently open desktops switch to the specified wallpaper

--- wallpaper/macOS.sh
+++ wallpaper/macOS.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#img_path=$(cd ~/.gvcci/themes/$1; pwd)/$(ls ~/.gvcci/themes/"$1"/ | grep wallpaper)
 img_path="$1"
-cmd="tell application \"Finder\" to set desktop picture to POSIX file \"$img_path\""
-echo "$cmd"
+cmd="tell application \"System Events\" to set picture of every desktop to POSIX file \"$img_path\""
 osascript -e "$cmd"
 
